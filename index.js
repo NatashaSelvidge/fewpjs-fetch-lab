a00cee70080d6b0df981e0e2848e023ef5467c45
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-02-24 16:31:18 -0800

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-
-
 function fetchBooks() {
   fetch("https://anapioficeandfire.com/api/books")
-    .then(r => r.json())
-    .then(json => renderBooks(json));
+    .then((r) => r.json())
+    .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
-  console.log(books)
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-
-  })
+  console.log(books);
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
 }
 
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks();
+});
